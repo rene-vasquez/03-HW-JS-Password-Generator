ae95cdbdd779d55d0a80a5bc48294e73ce6a6893
Rene Vasquez <rene4801@gmail.com>
2022-04-01 10:41:55 -0500

    Updated README and Script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 03-HW-JS-Password-Generator
+
+Big thank you to Stephen K. for helping on this HW.
+Shout out to Danny Yates as well for providing help
+We collaborated on this assignment on 3/31
+
+We essentially split the user input using the "split" function
+
+If the user included a character that correlated with one of our arrays it would list a new variable as true
+
+If that variable was true then it would add that array to our empty "choiceCharacters" array to generate a password from
+
+From then it takes the length input from the passwordLength prompt and generates a password using that number of characters.
--- script.js
+++ script.js
@@ -14,17 +14,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-  
 }
 
 function generatePassword () {
 
+//First three prompts that receive input from the end user
 alert ("Let's make a password!");
 var type = prompt ("Choose what type of characters you would like separated by commas \n L = 'Lowercase Characters' \n U = 'Uppercase Characters' \n N = 'Numbers' \n S = 'Special Characters'");
 var passwordLength = prompt ("How many characters would you like your password to be? \n Min: 8 Characters \n Max: 128 Characters");
-
+ 
+//This separates the user input into individual strings into the variable "typeChoices"
 var typeChoices = type.split(',')
 
+//setting up variables
 let password = "";
 
 var lowercaseChoice = "";
@@ -32,8 +34,7 @@ var uppercaseChoice = "";
 var numChoice = "";
 var speccharChoice = "";
 
-console.log (typeChoices);
-
+//CHecks if user included a certain set of characters
 if (typeChoices.includes("L")) {lowercaseChoice = true;}
 
 if (typeChoices.includes("U")) {uppercaseChoice = true;}
@@ -42,11 +43,13 @@ if (typeChoices.includes("N")) {numChoice = true;}
 
 if (typeChoices.includes("S")) {speccharChoice = true;}
 
+//Adds character sets to "choiceCharacters" variable
 lowercaseChoice ? (choiceCharacters += lowercase) : (choiceCharacters);
 uppercaseChoice ? (choiceCharacters += uppercase) : (choiceCharacters);
 numChoice ? (choiceCharacters += num) : (choiceCharacters);
 speccharChoice ? ( choiceCharacters += specchar) : (choiceCharacters);
 
+//generates password
 for (let i = 0; i < passwordLength; i++) {
   password += choiceCharacters.charAt(
     Math.floor(Math.random() * choiceCharacters.length)
